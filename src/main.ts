b65b61a5b87d58e0ed41bf697fcf19b7be60e1c8
wstypr <wsetypeermadi@gmail.com>
2024-10-01 20:41:53 +0700

    initial commit

--- src/main.ts
+++ src/main.ts
@@ -5,5 +5,6 @@ dotenv.config();
 
 const app = express();
 app.listen(process.env.PORT, () => {
+   // eslint-disable-next-line @/no-console
    console.log(`Server running on port ${process.env.PORT}`);
 });
