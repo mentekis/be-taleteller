be1f759478e1bf6a45152e5c28a8f3598c08cf68
wstypr <wsetypeermadi@gmail.com>
2024-10-02 08:36:41 +0700

    connect to mongodb

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,12 @@
-import express from "express";
 import dotenv from "dotenv";
-
 dotenv.config();
+import express from "express";
+import { connectDb } from "./utils/connectDb";
 
 const app = express();
+
+connectDb();
+
 app.listen(process.env.PORT, () => {
    // eslint-disable-next-line @/no-console
    console.log(`Server running on port ${process.env.PORT}`);
