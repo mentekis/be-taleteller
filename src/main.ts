7d868d365d3d630526304246d4b77037f665d622
wstypr <wsetypeermadi@gmail.com>
2024-10-07 11:57:02 +0700

    add cors

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
+import cors from "cors";
 
 dotenv.config();
 
@@ -14,6 +15,7 @@ const app = express();
 
 app.use(express.json());
 app.use(cookieParser());
+app.use(cors());
 
 app.use(authRouter);
 
