62b848512b8fab6f5f120f3123b3b70d6c305644
wstypr <wsetypeermadi@gmail.com>
2024-10-10 10:50:20 +0700

    set cors

--- src/main.ts
+++ src/main.ts
@@ -15,7 +15,12 @@ const app = express();
 
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors());
+app.use(
+   cors({
+      credentials: true,
+      origin: "*",
+   }),
+);
 
 app.use(authRouter);
 
